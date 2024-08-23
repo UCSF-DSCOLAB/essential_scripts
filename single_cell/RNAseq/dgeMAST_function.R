9b2b8d0ece51ad9c88f2dc76b4795610e28200b1
dtm2451 <daniel.bunis@ucsf.edu>
2024-08-23 13:09:45 -0400

    fix for no random effects & update documentation

--- single_cell/RNAseq/dgeMAST_function.R
+++ single_cell/RNAseq/dgeMAST_function.R
@@ -47,50 +47,60 @@ suppressPackageStartupMessages({
 #' \item \code{add.random.effect} must target discrete metadata and will be turned into a factor before use.
 #' \item 'ngeneson' (the number of expressed genes per cell, which will be calculated internally) and \code{dge.group.by} are automatically added as fixed effects and do not need to be added here.
 #' }
-#' 
+#'
+#' @return a data.table
 #' @details This function loops across multiple clusters running DGE between 2 groups, gathering eveything into a single data.frame.
 #' Think like FindAllMarkers, but for a biological question per each cluster.
-#' 
+#'
 #' Raw counts (integers) are expected to exist and to be in the 'counts' slot of the 'RNA' assay if \code{object} is a Seurat object, or in an assay named 'counts' if \code{object} is a SingleCellExperiment object.
-#' 
+#'
 #' In preparation:
-#' 
+#'
 #' Seurat data is converted to a SingleCellObject (SCE).
 #' The SCE is trimmed to only the cells indicated by \code{cells.use}, and only those where \code{dge.group.by} is either \code{dge.group.1} or \code{dge.group.2}.
 #' And the set of cell targets given by \code{cells.group.targets} is trimmed of any targets that don't have any cells of code{dge.group.1} or \code{dge.group.2}
-#' 
-#' Then, the function loops through remaining \code{cells.group.targets}, performing these steps on just that subset of cells:
-#' 
+#'
+#' Then, the function loops through \code{cells.group.targets}, performing these steps per each subset of cells:
+#'
 #' Log and read-depth normalization of the counts data,
 #' trimming genes expressed in fewer than \code{mast.freq.expressed.min} proportion of cells,
 #' calculation of the number of genes expressed per cell ('ngeneson'),
-#' the DGE calculation by MAST with its zlm function with \code{method='glmer', ebayes = F, strictConvergence = FALSE, fitArgsD = list(nAGQ = 0)} and a \code{formula} based on the contents of \code{add.random.effect} and \code{add.fixed.effect}, followed by running the liklihood ratio test on the contrast of interest.
-#' By default, the formula ammounts to \code{~ <dge.group.by> + ngeneson + (1 | orig.ident)}.
+#' the DGE calculation with \code{formula} based on the contents of \code{add.random.effect} and \code{add.fixed.effect}.
+#' By default, the formula amounts to \code{~ <dge.group.by> + ngeneson + (1 | orig.ident)}.
 #' FDR-correction is applied to p-values at this stage -- per each cell type.
-#' 
+#'
 #' It then renames columns, converts the column holding fold change information to be base2, and adds an "up_in" column to help explain which code{dge.group.1} or \code{dge.group.2} was found to have higher expression for each gene.
-#' @return a data.frame
+#' @section DGE methodology details:
+#' DGE is run by first modeling the data using MAST's zlm function, followed by determining p-values using its likelihood ratio test methodology on the contrast of interest.
+#'
+#' Specifically, \code{zlm} is run with settings: \itemize{
+#' \item Always: \code{ebayes = FALSE, strictConvergence = FALSE}
+#' \item Additionally:\itemize{
+#'   \item when there is a random effect given (default) to \code{add.random.effect}: \code{method='glmer', fitArgsD = list(nAGQ = 0)}
+#'   \item when there is no random effect given to \code{add.random.effect}: \code{method='glm'}
+#' }
+#' }
 #' @author Daniel Bunis and Ravi Patel
 dgeMAST <- function(
   object,
-  cells.group.by, # metadata name holding clusters or annotations to explore within
-  cells.group.targets = NULL, # Optional but RECOMMENDED. String ("T4 naive" / "0") or strings ('c("T4 naive", "T8 naive")' / 'c(1,3)'). If not changed from the default here, will loop through all annotations/clusters given by 'cells.group.by'.
-  dge.group.by, # metadata column holding samples'/cell's groupings
-  dge.group.1, # The "primary" group = when foldChange is positive, it means it's up in this population. 
-  dge.group.2, # The "other" group
-  cells.use = NULL, # NULL or Logical vector (length equal to the number of cells in object) indicating cells to keep (TRUE) or remove (FALSE) before calculating DGE.
-  log.prefix = '', # String to append at the beginning of log messages.  Useful when wrapping this function inside an external loop.
-  mast.freq.expressed.min = 0.2, # Fraction. A gene filter before dge calculation: Minimal percent of cells (among targets groups, within each targetted cell set) that must express a gene for it to be considered.
-  add.random.effect = "orig.ident", # NULL or a String vector naming a metadata to treat as a random effects in the mixed effect model used for DGE calculation. These metadata must contain discrete data and will be turned into a factor before use. Providing batch information is highly recommended, and is what the default of "orig.ident" aims to capture. 
-  add.fixed.effect = NULL # NULL or a String vector naming a metadata to treat as fixed effects in the mixed effect model used for DGE calculation. Note: You do not need to provide 'dge.group.by' here, and the number of expressed genes will be calculated and stored/used as 'ngeneson'; Both will be added as fixed effects and do not need to be named here.
+  cells.group.by,
+  cells.group.targets = NULL,
+  dge.group.by,
+  dge.group.1,
+  dge.group.2,
+  cells.use = NULL,
+  log.prefix = '',
+  mast.freq.expressed.min = 0.2,
+  add.random.effect = "orig.ident",
+  add.fixed.effect = NULL
 ) {
 
   log_message <- function(...) {
-    cat("[", format(Sys.time()), "]", ..., "\n")
+    cat("[", format(Sys.time()), "]", ..., "\n", sep = "")
   }
 
-  if (!isMeta(cells.group.by, object)) stop("'cells.group.by', ", cells.group.by,", is not a metadata of 'object'")
-  if (!isMeta(dge.group.by, object)) stop("'dge.group.by', ", dge.group.by,", is not a metadata of 'object'")
+  if (!dittoSeq::isMeta(cells.group.by, object)) stop("'cells.group.by', ", cells.group.by,", is not a metadata of 'object'")
+  if (!dittoSeq::isMeta(dge.group.by, object)) stop("'dge.group.by', ", dge.group.by,", is not a metadata of 'object'")
   if (!dge.group.1 %in% object[[dge.group.by, drop = TRUE]]) stop("No cells have 'dge.group.1', ", dge.group.1, ", as their 'dge.group.by' value.")
   if (!dge.group.2 %in% object[[dge.group.by, drop = TRUE]]) stop("No cells have 'dge.group.2', ", dge.group.2, ", as their 'dge.group.by' value.")
   if (!is.null(cells.group.targets) && !all(cells.group.targets %in% object[[cells.group.by, drop = TRUE]])) stop("At least one target of 'cells.group.targets' does not exist within 'cells.group.by' values.")
@@ -103,7 +113,7 @@ dgeMAST <- function(
   }
 
   # Trim to only cells passing cells.use
-  if (!is.null(cells.use)) {
+  if (!identical(cells.use, NULL)) {
     sce <- sce[, dittoSeq:::.which_cells(
       cells.use,
       sce)]
@@ -120,7 +130,7 @@ dgeMAST <- function(
 
   # Establish cell targets if not given
   if (is.null(cells.group.targets)) {
-    cell.group.targets <- metaLevels(cells.group.by, sce)
+    cells.group.targets <- dittoSeq::metaLevels(cells.group.by, sce)
   }
 
   # Check for any lack of cell groups, and trim cell targets if so
@@ -183,13 +193,22 @@ dgeMAST <- function(
       }
 
       # Run MAST
+      zlm_args <- list(
+          formula = as.formula(model),
+          sca = sca_ct,
+          exprs_values = 'logcounts',
+          ebayes = FALSE,
+          strictConvergence = FALSE,
+          method = 'glm'
+      )
+      if (!identical(add.random.effect, NULL)) {
+          zlm_args$method = 'glmer'
+          zlm_args$fitArgsD = list(nAGQ = 0)
+      }
+
+      log_message(log.prefix, "\twith model formula: ", model)
       contrast_val <- paste0(dge.group.by, dge.group.1)
-      zlmCond <- zlm(
-        as.formula(model),
-        sca_ct, exprs_value = 'logcounts',
-        method='glmer', ebayes = F,
-        strictConvergence = FALSE,
-        fitArgsD = list(nAGQ = 0))
+      zlmCond <- do.call(zlm, zlm_args)
       summaryCond <- summary(zlmCond, doLRT=contrast_val)$datatable
       fcHurdle <- merge(
         summaryCond[contrast==contrast_val & component=='H',.(primerid, `Pr(>Chisq)`)], #hurdle P values
