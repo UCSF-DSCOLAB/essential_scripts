99ab50edd0686a2acf29e9467d62ec1e93a58fbe
Ravi Patel <ravipatel4@gmail.com>
2026-02-27 11:45:07 -0800

    Modified deseq2 function to remove input checks and added an input_checks_function.R

--- single_cell/dge/deseq2_function.R
+++ single_cell/dge/deseq2_function.R
@@ -1,7 +1,9 @@
 library(edgeR)
 library(DESeq2)
 library(tidyverse)
-##### DGE analysis using DESeq2
+### DGE analysis using DESeq2
+#
+#' Run DGE between 2 groups using DESeq2
 #' @param counts: feature (row) x sample (column) raw count matrix
 #' @param metadata: sample metadata, with rownames containing column names of \code{counts}
 #' @param dge_by: name of \code{metadata} column to use DGE comparion. Must have exactly two levels
@@ -9,31 +11,13 @@ library(tidyverse)
 #' @param reference_group: level to be used as denominator in log2FC calculation
 #' @param fixed_effects: a vector of \code{metadata} column names to use as fixed effects
 run_deseq2 <- function(counts, metadata, dge_by, case_group, reference_group, fixed_effects=NULL) {
-  if(! all( colnames(counts) %in% rownames(metadata)) ) {
-    stop("One or more samples in counts are not present in metadata.")
-  }
-  # Subset and order metadata according to the order of samples in counts
-  metadata = metadata[ match(colnames(counts), rownames(metadata)) , ]
-  
-  if( length(unique(as.vector(metadata[,dge_by]))) != 2) {
-    stop("Comparison column of metadata does not have exactly two unique values.")
-  }
-  if( ! case_group %in% metadata[,dge_by] ) {
-    stop("Case group does not exist in dge_by column of metadata.")
-  }
-  if( ! reference_group %in% metadata[,dge_by] ) {
-    stop("Reference group does not exist in dge_by column of metadata.")
-  }
-  if(! is.null(fixed_effects) & ! all(fixed_effects %in% colnames(metadata)) ) {
-    stop("One or more fixed_effects do not exist in metadata.")
-  }
 
-  # Keep features that are expressed with CPM > 1 in more than 50% of samples in at least one comparison group.
-  # feats = names(which(apply(cpm(counts) > 1, 1, function(x) { 
-  #                                            any(tapply(x, metadata[,dge_by], mean) > 0.5) 
-  #                                          } )
-  #                    )
-  #              )
+
+  # Commong input checks
+  metadata = input_checks(counts, metadata, dge_by, case_group, reference_group, fixed_effects)
+
+  # Method specific input checks
+  # None for DESeq2
 
   # Determine the number of sample in the smaller group (min_gsize) and keep features that are expressed with CPM > 1 in at least 60% of min_gsize samples, regardless of the sample group identity.
   min_gsize = min(table(metadata[,dge_by]))
