524c0e812acd0a017e4cd7a461dea94a8ac10bab
Ravi Patel <ravipatel4@gmail.com>
2026-03-13 11:54:14 -0700

    allow contrasts as input: input_checks_function.R

--- single_cell/dge/deseq2_function.R
+++ single_cell/dge/deseq2_function.R
@@ -3,22 +3,22 @@ library(DESeq2)
 library(tidyverse)
 ### DGE analysis using DESeq2
 #
-#' Run DGE between 2 groups using DESeq2
-#' @param counts: feature (row) x sample (column) raw count matrix.  Only samples intended for DGE analysis should be included in \code{counts}
-#' @param metadata: sample metadata, with rownames containing column names of \code{counts}
-#' @param dge_by: name of \code{metadata} column to use DGE comparion. Must have exactly two levels
-#' @param case_group: level to be used as numerator in log2FC calculation
-#' @param reference_group: level to be used as denominator in log2FC calculation
-#' @param fixed_effects: a vector of \code{metadata} column names to use as fixed effects
+#' Run DGE between 2 groups using DESeq2.
+#' @param counts A feature (row) x sample (column) raw count matrix.  Only samples intended for DGE analysis should be included in \code{counts}.
+#' @param metadata Sample metadata, with rownames containing column names of \code{counts}.
+#' @param dge_by Name of \code{metadata} column to use for DGE comparion. Must have exactly two levels.
+#' @param case_group Group to be used as numerator in log2FC calculation.
+#' @param reference_group Group to be used as denominator in log2FC calculation.
+#' @param fixed_effects A vector of \code{metadata} column names to use as fixed effects.
 run_deseq2 <- function(counts, metadata, dge_by, case_group, reference_group, fixed_effects=NULL) {
 
   # Commong input checks
-  metadata = input_checks(counts, metadata, dge_by, case_group, reference_group, fixed_effects)
+  .input_checks(counts, metadata, dge_by, case_group, reference_group, fixed_effects)
 
   # Method specific input checks
   # None for DESeq2
 
-  counts = remove_low_expression(counts, metadata, dge_by, min_frac=0.6)
+  counts = .remove_low_expression(counts=counts, metadata=metadata, dge_by=dge_by, dge_groups=c(case_group, reference_group), min_frac=0.6)
   
   # Make formula string
   formula_str = paste("~", dge_by)
